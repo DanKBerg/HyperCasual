8e3e32dca5401a75313f2d49985b4e345a6dd239
Daniel Karlsrud Berg <daniel.karlsrud.berg@gmail.com>
2021-02-16 00:16:14 -0500

    Fixed flinging mechanic
    I fixed the flinging mechanic with a line of code that Brianna created for me. I have no idea what it does. I also made the empty gameobject instantiate bricks at the start of a touch and not spawn a brick before the start of a new touch. I also added the brick flying forward.

---
+++ Hypercasual Project/Assets/Scripts/BrickForce.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickForce : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public Vector3 flingBrickSnyder;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.AddForce(flingBrickSnyder);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Hypercasual Project/Assets/Scripts/BrickSpawner.cs
+++ Hypercasual Project/Assets/Scripts/BrickSpawner.cs
@@ -4,26 +4,48 @@ using UnityEngine;
 
 public class BrickSpawner : MonoBehaviour
 {
-    private float startingZPosition;
+    public GameObject brickSnyder;
+
+    public Camera cam;
+
+    private bool spawnBrick = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        startingZPosition = transform.position.z;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
-        if(Input.touchCount > 0)
+        //If the user touches the screen.
+        if (Input.touchCount > 0)
         {
+            //Holds the information of the first touch
             Touch touch = Input.GetTouch(0);
-            Debug.Log("Touched");
-            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.rawPosition);
-            Debug.Log(touchPosition);
-            Debug.Log(touch.position);
-            touchPosition.z = startingZPosition;
+
+            //Brianna will explain this
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, Camera.main.nearClipPlane + 7f));
+
+            //Sets the transform position of the gameobject to the position of the touch.
+            transform.position = touchPosition;
+
+            //If the touch has started and you can spawn a brick
+            if (touch.phase == TouchPhase.Began)
+            {
+                //Spawns a brick
+                Instantiate(brickSnyder, transform.position, transform.rotation);
+
+                spawnBrick = false;
+            }
+
+            //If the touch ends...
+            if (touch.phase == TouchPhase.Ended)
+            {
+                //...spawnBrick will be set to true.
+                spawnBrick = true;
+            }
         }
     }
 }
