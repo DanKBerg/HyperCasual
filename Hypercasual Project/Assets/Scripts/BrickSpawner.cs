8fdcc94e998c22bda372649d312447aa4f65672c
Daniel Karlsrud Berg <daniel.karlsrud.berg@gmail.com>
2021-02-15 22:45:01 -0500

    Trying to implement flinging mechanic
    Trying to implement flinging mechanic but it will only give one value as touch.position is fixed to one value.

---
+++ Hypercasual Project/Assets/Scripts/BrickSpawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BrickSpawner : MonoBehaviour
+{
+    private float startingZPosition;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingZPosition = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            Debug.Log("Touched");
+            Vector3 touchPosition = Camera.main.ScreenToWorldPoint(touch.rawPosition);
+            Debug.Log(touchPosition);
+            Debug.Log(touch.position);
+            touchPosition.z = startingZPosition;
+        }
+    }
+}
